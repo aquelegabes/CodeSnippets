d4134430e32062c51324b712ab9aa357d8aa4876
Gabriel Santos <34673909+gabesantos1@users.noreply.github.com>
2019-02-11 14:14:25 -0200

    adição apicaller response

---
+++ C#/ApiCaller/ApiCallerResponse.cs
@@ -0,0 +1,29 @@
+using Newtonsoft.Json;
+using System.Net;
+
+
+namespace Insert.Your.Namespace.Here
+{
+    public class ApiCallerResponse
+    {
+        public HttpStatusCode StatusCode { get; set; }
+        public string ResponseText { get; set; }
+        /// <summary>
+        /// Retorna um objeto serializado escolhido, caso falhe retornará o próprio objeto, mas seu valor será nulo.
+        /// </summary>
+        /// <typeparam name="T">Entity</typeparam>
+        /// <returns>Entity</returns>
+        public dynamic DeserializedObject<T>()
+        {
+            try
+            {
+                return JsonConvert.DeserializeObject<T>(ResponseText);
+            }
+            catch
+            {
+                return default(T);
+            }
+        }
+
+    }
+}
