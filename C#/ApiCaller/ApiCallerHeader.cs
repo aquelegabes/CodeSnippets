3facb67bb308e51a704f91ab185ef87033e1f79f
Gabriel Santos <34673909+gabesantos1@users.noreply.github.com>
2019-02-11 14:13:52 -0200

    adição apicallerheader

---
+++ C#/ApiCaller/ApiCallerHeader.cs
@@ -0,0 +1,9 @@
+using System.Net;
+namespace Insert.Your.Namespace.Here
+{
+    public class ApiCallerHeader
+    {
+        public HttpRequestHeader Key { get; set; }
+        public string Value { get; set; }
+    }
+}
