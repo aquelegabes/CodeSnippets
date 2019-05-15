e54b9dc8bea1f0967e1752e75b121e46e93b9103
Gabriel Santos <34673909+gabesantos1@users.noreply.github.com>
2019-05-15 15:34:58 -0300

    Update dateUtils.cs

--- C#/Utilidades/dateUtils.cs
+++ C#/Utilidades/dateUtils.cs
@@ -1,21 +1,22 @@
+// Get weekends days in date
 private int GetWeekendsInDate(DateTime startDate)
+{
+    int result = 0;
+    startDate = new DateTime(startDate.Year, startDate.Month, 1);
+    int daysinmonth = DateTime.DaysInMonth(startDate.Year, startDate.Month);
+    var endDate = new DateTime(startDate.Year, startDate.Month, daysinmonth);
+    TimeSpan diff = endDate - startDate;
+    int days = diff.Days;
+    for (var i = 0; i<=days; i++)
     {
-        int result = 0;
-        startDate = new DateTime(startDate.Year, startDate.Month, 1);
-        int daysinmonth = DateTime.DaysInMonth(startDate.Year, startDate.Month);
-        var endDate = new DateTime(startDate.Year, startDate.Month, daysinmonth);
-        TimeSpan diff = endDate - startDate;
-        int days = diff.Days;
-        for (var i = 0; i<=days; i++)
+        var auxdate = startDate.AddDays(i);
+        switch (auxdate.DayOfWeek)
         {
-            var auxdate = startDate.AddDays(i);
-            switch (auxdate.DayOfWeek)
-            {
-                case DayOfWeek.Saturday:
-                case DayOfWeek.Sunday:
-                    result++;
-                    break;
-            }
+            case DayOfWeek.Saturday:
+            case DayOfWeek.Sunday:
+                result++;
+                break;
         }
-        return result;
     }
+    return result;
+}
