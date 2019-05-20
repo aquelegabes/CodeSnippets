aab9e31fef38cd439bee8614799967286e7159ec
Gabriel Santos <34673909+gabesantos1@users.noreply.github.com>
2019-05-20 16:56:36 -0300

    Create list_files_recursive.py

---
+++ Python/list_files_recursive.py
@@ -0,0 +1,12 @@
+import os
+
+def listar_pasta(path):
+	if os.path.isdir(path):
+		subpastas = list(filter(lambda x: os.path.isdir(os.path.join(path,x)), os.listdir()))
+		for pasta in subpastas:
+			listar_pasta(path)
+	else:
+		print (list(filter(lambda x: not os.path.isdir(os.path.join(path,x)),os.listdir())))
+
+
+listar_pasta('C:\\Users\\gabriel.santana\\Documents\\Gabriel Santos\\etc\\prova')
