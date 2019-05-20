2122970b2d8812fd58231aad7a5360dcd6565063
Gabriel Santos <34673909+gabesantos1@users.noreply.github.com>
2019-05-20 16:59:43 -0300

    Update list_files_recursive.py

--- Python/list_files_recursive.py
+++ Python/list_files_recursive.py
@@ -4,7 +4,7 @@ def listar_pasta(path):
 	if os.path.isdir(path):
 		subpastas = list(filter(lambda x: os.path.isdir(os.path.join(path,x)), os.listdir()))
 		for pasta in subpastas:
-			listar_pasta(path)
+			listar_pasta(os.path.join(path,pasta))
 	else:
 		print (list(filter(lambda x: not os.path.isdir(os.path.join(path,x)),os.listdir())))
 
