d056f08914d4f6c6582809b722f09322acfa29ef
Gabriel Santos <34673909+gabesantos1@users.noreply.github.com>
2019-05-28 13:31:32 -0300

    altered to contains utilities functions

--- Python/utils.py
+++ Python/utils.py
@@ -10,3 +10,12 @@ def read_files(path):
 			read_files(os.path.join(path,pasta))
 	print (path)
 	print (list(filter(lambda x: not os.path.isdir(os.path.join(path,x)), os.listdir(path))))
+
+def rule_of_three(mult1, mult2, div):
+	"""
+		Solves rule of three
+		mult1 = first number to multiply
+		mult2 = second number to multiply
+		div   = number to divide the previous result
+	"""
+	return mult1 * mult2 / div
